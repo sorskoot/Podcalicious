366d5da1f78157878e15c4c84ed5232ce23350b3
sorskoot <mental_c@hotmail.com>
2017-03-04 17:50:16 +0100

    redirect to https

--- app.js
+++ app.js
@@ -31,17 +31,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/api',api);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
+  if (!req.secure) {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
